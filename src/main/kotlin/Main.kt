8ce9cd128fef0e4641e07799dde498d05a6df066
Adrián Robotka <robotka@sch.bme.hu>
2023-04-19 23:23:46 +0200

    authors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
+import net.pproj.tutter.api.AuthorApi
+
 fun main() {
-    println("Hello World a!")
+    println("Hello, world!")
+    val baseUrl = "http://localhost:8080/api"
+    val authors = AuthorApi(baseUrl).authorGet()
+    println(authors)
 }
