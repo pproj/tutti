6ac1c8e2cdc8910fc5e4f07eec9c3f21474b0ca2
Adrián Robotka <robotka@sch.bme.hu>
2023-04-20 15:49:43 +0200

    build cleanup

--- buildSrc/src/main/kotlin/TuttiBuildPlugin.kt
+++ buildSrc/src/main/kotlin/TuttiBuildPlugin.kt
@@ -7,12 +7,7 @@ class TuttiBuildPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val tasks = project.tasks
         val downloadSpec = tasks.register<GitRepoGetter>("downloadTutterRepo")
-//        val modelGen = tasks.register<ApiModelGenerator>("generateApiModels")
-//        modelGen.configure {
-//            val apiSpecPath = "apidoc/spec.yaml"
-//            specFile.set(downloadSpec.flatMap { it.repoPath }.map { it.resolve(apiSpecPath) })
-//            dependsOn(downloadSpec)
-//        }
+
         tasks.getByName("compileKotlin").dependsOn(downloadSpec)
     }
 }
