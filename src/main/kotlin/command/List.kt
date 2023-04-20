5133e223695e6d962ea1c05978489719b86eed84
Adrián Robotka <robotka@sch.bme.hu>
2023-04-20 15:44:37 +0200

    filtering

--- src/main/kotlin/command/List.kt
+++ src/main/kotlin/command/List.kt
@@ -4,10 +4,16 @@ import Config
 import ago
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.check
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.enum
+import com.github.ajalt.clikt.parameters.types.int
 import net.pproj.tutter.api.AuthorApi
 import net.pproj.tutter.api.PostApi
 import net.pproj.tutter.api.TagApi
+import net.pproj.tutter.client.infrastructure.ClientError
+import net.pproj.tutter.client.infrastructure.ClientException
 import net.pproj.tutter.model.PostWithAuthor
 
 enum class ListSubject {
@@ -17,7 +23,18 @@ enum class ListSubject {
 val subjects = ListSubject.values().joinToString(", ") { it.name.toLowerCase() }
 
 class List : CliktCommand(help = "Listing for $subjects") {
+    // What to list
     private val subject by argument().enum<ListSubject>()
+
+    /*
+     * Filtering options
+     */
+    private val limit by option(
+        "-n", "--limit",
+        help = "Limit on the number of displayed items."
+    ).int().default(10).check { it > 1 }
+    private val tag by option("-t", "--tag", help = "Tag to filter by. (without '#') (e.g. --tag kotlin)")
+    private val author by option("-a", "--author", help = "Author's name to filter by.")
 
     override fun run() {
         try {
@@ -28,6 +45,12 @@ class List : CliktCommand(help = "Listing for $subjects") {
             }
         } catch (e: Exception) {
             echo("Cannot list ${subject}: ${e.message}")
+            if (e is ClientException) {
+                val resp = e.response
+                if (resp is ClientError<*>) {
+                    echo(resp.body)
+                }
+            }
         }
     }
 
@@ -39,9 +62,26 @@ class List : CliktCommand(help = "Listing for $subjects") {
         echo("#{it.tag} (first seen ${it.firstSeen})")
     }
 
-    private fun listTutts() = PostApi(Config.baseUrl).postGet().forEach {
-        echo(it.humanFriendly())
-        echo("---")
+    private fun listTutts() = PostApi(Config.baseUrl)
+        .postGet(
+            limit = limit,
+            tag = tag,
+            authorId = authorId(author)
+        )
+        .forEach {
+            echo(it.humanFriendly())
+            echo("---")
+        }
+
+    private fun authorId(author: String?): Int? {
+        if (author == null)
+            return null
+
+        val record = AuthorApi(Config.baseUrl)
+            .authorGet()
+            .find { it.name == author } ?: throw IllegalArgumentException("Author '$author' does not exist.")
+
+        return record.id
     }
 }
 
